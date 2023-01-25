2244cc9d52a262c7f7494af2c2205187469cc1d3
Sean Gray <seangray.dev@gmail.com>
2023-01-25 20:07:57 +0000

    added tail.js

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: [${actual}] === [${expected}]`;
+  } else {
+    return `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  }
+};
+
+const tail = function (array) {
+  if (array.length <= 1) {
+    return [];
+  } else {
+    return array.slice(1);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
