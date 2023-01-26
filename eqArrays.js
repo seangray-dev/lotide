2afd3abb4d39cc763d5adbbd4911bb6bc016c47b
Sean Gray <seangray.dev@gmail.com>
2023-01-26 23:14:50 +0000

    update eqArray.js function

--- eqArrays.js
+++ eqArrays.js
@@ -12,7 +12,7 @@ const eqArrays = function (actual, expected) {
   } else {
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] !== expected[i]) {
-        return true;
+        return false;
       }
     }
     return true;
@@ -25,3 +25,6 @@ eqArrays(["1", "2", "3"], ["1", "2", "3"]);
 eqArrays(["1", "2", "3"], ["1", "2", 3]);
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
