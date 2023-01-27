3dd11076a7ae556592da1452af1f3faf5723b527
Sean Gray <seangray.dev@gmail.com>
2023-01-27 01:07:30 +0000

    added middle.js helper function

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: [${actual}] === [${expected}]`;
+  } else {
+    return `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  }
+};
+
+const middle = function (array) {
+  if (array.length <= 2) {
+    return [];
+  }
+  if (array.length % 2 === 0) {
+    return [array[array.length / 2 - 1], array[array.length / 2]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
+};
+
+middle([1]); // => []
+middle([1, 2]); // => []
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+console.log(eqArrays(middle([1, 2, 3]), middle([1, 2, 3])));
+
