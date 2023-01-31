53b81dc1e0bf0cb79ec121e8acdc309f8a71bf2e
Sean Gray <seangray.dev@gmail.com>
2023-01-31 20:55:54 +0000

    added findKeyByValue.js lotide helper function

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: [${actual}] === [${expected}]`;
+  } else {
+    return `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  }
+};
+
+function findKeyByValue(obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
