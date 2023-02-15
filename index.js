130b07ee8672540221a72bfa6f0ba5cb0d2be58e
Sean Gray <seangray.dev@gmail.com>
2023-02-15 14:42:18 +0000

    added all util functions to export

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const head = require("./head");
-const tail = require("./tail");
-const middle = require("./middle");
-const assertEqual = require("./assertEqual");
 const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findkey = require("./findkey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const head = require("./head");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require("./middle");
+const tail = require("./tail");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
   assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findkey: findkey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without,
 };
