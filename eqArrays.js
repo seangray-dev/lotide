27947ac9f46f157259866448e122fbf173409b8f
Sean Gray <seangray.dev@gmail.com>
2023-02-16 03:35:35 +0000

    use recursion to handle nested arrays/objects for eqArrays & eqObjects functions

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,21 @@
-const eqArrays = function (actual, expected) {
-  if (actual.length !== expected.length) {
+const eqArrays = function (arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
     return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
     }
-    return true;
   }
+  return true;
 };
 
 module.exports = eqArrays;
--- eqObjects.js
+++ eqObjects.js
@@ -5,19 +5,30 @@ const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
+
   // loop through object1 keys
   for (const key in object1) {
+    // check if current value of current key is an object
+    if (typeof object1[key] === "object" && object1[key] !== null) {
+      // recursively call the object if it is an object
+      return eqObjects(object1[key], object2[key]);
+    }
+    // if object 2 doens't have same key, return false
+    if (!object2.hasOwnProperty(key)) {
+      return false;
+    }
     // check if both objects' keys are arrays
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       // if they are, use eqArrays to compare the arrays
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+      // if values of keys are not the same return false
     } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
-
+  // return true if all checks pass
   return true;
 };
 
